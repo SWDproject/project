34449a27e071898a20568da785ccd58ababa9885
Hadeel <p7079889@gmail.com>
2025-05-17 17:53:09 +0300

    Add review schedule files

--- show_review_schedule.sh
+++ show_review_schedule.sh
@@ -3,7 +3,7 @@ show_review_schedule(){
 echo "enter the subject name: "
 read subject
 file="review_${subject}.txt"
-if [! -f "$file" ]; then
+if [ ! -f "$file" ]; then
 echo "No review schedule found for subject $subject "
 return
 fi
@@ -12,11 +12,11 @@ echo "-------------------------------------"
 awk '
 BEGIN { RS"---"; FS="\n" }
 {
-for (i=1; i<NF; i++) {
+for (i=1; i<=NF; i++) {
 print $1
 }
 print "-------------------------------------"
 }
 ' "$file"
 }
-
+show_review_schedule
