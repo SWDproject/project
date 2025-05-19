7eba2ef231412aa98acec1e6c3e8b71e5780fe45
Najd-Aljoudi <najdaljoudi@gmail.com>
2025-05-19 10:21:58 +0000

    add create_account.sh function and separate file per user

--- create_account.sh
+++ create_account.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
-# This function creates a new user account and stores the username with a hashed password
-create_account() {
-  USER_FILE="users.txt"
+# This function creates a new user account by saving the hashed password in a separate file named after the username inside the 'users' directory.
 
+create_account() {
+  USERS_DIR="users"
+  
+  # Create 'users' directory if it doesn't exist
+  mkdir -p "$USERS_DIR"
+  
   echo "  Create New Account  "
+  
+  # Ask the user to enter a username and password
   read -p "Enter username: " username
   read -s -p "Enter password: " password
   echo
-
-  # Check if the users.txt exists, if not create it
-  if [ ! -f "$USER_FILE" ]; then
-    touch "$USER_FILE"
-  fi
+  
+  USER_FILE="$USERS_DIR/$username.txt"
+  
 
   # Check if the username already exists
-  if grep -q "^$username:" "$USER_FILE"; then
+  if [  -f "$USER_FILE" ]; then
     echo "Username already exists. Please choose another one."
     return 1
   fi
 
+ 
   # Hash the password using SHA-256
   hashed_password=$(echo -n "$password" | sha256sum | awk '{print $1}')
 
-  # Save the username and hashed password
-  echo "$username:$hashed_password" >> "$USER_FILE"
+  # Save the hashed password in the user's file
+  echo "password:$hashed_password" > "$USER_FILE"
   echo "Account created successfully"
 }
+
+create_account
