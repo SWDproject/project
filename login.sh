a4ba46b5d99018742c6492abbd25f7e0682addb8
Najd-Aljoudi <najdaljoudi@gmail.com>
2025-05-19 10:46:24 +0000

    add login.sh function

--- login.sh
+++ login.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# This function verifies user login by checking the username and hashed password
+# This function verifies user login by checking the username and matchig the hashed password stored in a separate file created for eche user inside the 'users' directory.
+
 login() {
-  USER_FILE="users.txt"
+  USERS_DIR="users"
 
   echo "  Login  "
+  
+  # Ask the user to enter a username and password
   read -p "Enter username: " username
   read -s -p "Enter password: " password
   echo
+  
+  USER_FILE="$USERS_DIR/$username.txt"
 
-  # Check if the users.txt exists
+  # Check if the user file exists (i.e. account exists)
   if [ ! -f "$USER_FILE" ]; then
     echo "No account found. Please create an account first."
     return 1
@@ -17,11 +22,16 @@ login() {
 
   # Hash the entered password 
   hashed_password=$(echo -n "$password" | sha256sum | awk '{print $1}')
-
-  # Check if the combination exists in users.txt
-  if grep -q "^$username:$hashed_password$" "$USER_FILE"; then
+  
+  # Extract the stored hashed password from the user file
+  stored_password=$(grep "^password:" "$USER_FILE" | cut -d':' -f2)
+  
+  # Compare the stored hashed password from the user file 
+  if [ "$hashed_password" == "$stored_password" ]; then
     echo "Login successful"
   else
     echo "Invalid username or password."
   fi
 }
+
+login
